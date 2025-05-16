ced92f1146b9da36a42ef22eed77e31103d13ea5
Soham Mayur <sohammayurroll@gmail.com>
2025-05-16 18:48:14 +0530

    Update binary.c

--- binary.c
+++ binary.c
@@ -1,40 +1,37 @@
-#include<stdio.h>
-int main(){
-    int a[10],i,s,lb,ub,mid,flag=0;
-printf("\n Enter the values into array in ascending order");
-for(i=0;i<10;i++)
-{
-    scanf("%d", &a[i]);
-}
-printf("\n Enter the search eliment");
-scanf("%d",&s);
-lb=0;
-ub=3;
-while(lb<=ub)
-{
-    mid=(lb+ub)/2;
-    if(s==a[mid])
-    {
-        flag=1;
-        break;
+#include <stdio.h>
+
+int main() {
+    int a[10], i, s, lb, ub, mid, flag = 0;
+
+    printf("\nEnter the values into array in ascending order:\n");
+    for (i = 0; i < 10; i++) {
+        scanf("%d", &a[i]);
     }
-    else if(s>a[mid])
-    {
-        lb=mid+1;
+
+    printf("\nEnter the search element: ");
+    scanf("%d", &s);
+
+    lb = 0;
+    ub = 9;  
+
+    while (lb <= ub) {
+        mid = (lb + ub) / 2;
+        if (s == a[mid]) {
+            flag = 1;
+            break;
+        } else if (s > a[mid]) {
+            lb = mid + 1;
+        } else {
+            ub = mid - 1;
+        }
     }
-    else
-    {
-        ub=mid-1;
+
+    if (flag == 1) {
+        printf("The search is successful at position %d\n", mid + 1);
+    } else {
+        printf("Search is not successful\n");
     }
+
+    return 0;
 }
-//end of while
-if(flag==1)
-{
-    printf("the search is successful at %d",(mid+1));
-}
-else
-{
-    printf("search is not successful");
-}
-return 0;
-}
+
